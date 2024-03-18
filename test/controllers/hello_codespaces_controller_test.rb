5389fc444f8ef294d3fbdf183035af6e65be4b40
Kendall Gassner <kendallgassner@github.com>
2024-03-18 19:59:54 +0000

    test yield

--- test/controllers/hello_codespaces_controller_test.rb
+++ test/controllers/hello_codespaces_controller_test.rb
@@ -4,5 +4,6 @@ class HelloCodespacesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get "/"
     assert_response :success
+    assert_select "h1", text: "Hello welcome to the dashboard!"
   end
 end
